0640b1a038bffa36eb7c620b97a3a110783f2c63
MatteoSbarluzzi <matteosbarluzzi1@gmail.com>
2024-12-26 17:04:10 +0100

    Selfwork Operatori 1

---
+++ main1.js
@@ -0,0 +1,10 @@
+let num1 = 100;
+let num2 = 200;
+let num3 = 300;
+let num4 = 400;
+let num5 = 500;
+
+let somma = num1 + num2 + num3 + num4 + num5;
+let media = somma / 5;
+
+console.log( `La somma tra i numeri corrisponde a ${somma} e la media equivale a ${media}`);
